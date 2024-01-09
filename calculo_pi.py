e6781324f5b5c558719549fbd6f9abd837d6bf20
Edgar Gonzalez Ambriz <egonzalezambriz@gmail.com>
2024-01-09 13:50:56 -0600

    Se cambio el nombre variable numero_de_agujas y se agregaron mas comentarios

--- calculo_pi.py
+++ calculo_pi.py
@@ -1,39 +1,43 @@
 
 # Estimacion de PI a traves de un metodo estocastico
 
+# importar librerias
 import random
 import math
 from estadisticas import desviacion_estandar, media
 
 
 ###################################################################
-def aventar_agujas (numero_de_agujas) :
+def aventar_agujas (numero_agujas) :
 ###################################################################
     dentro_del_circulo = 0
 
-    for _ in range (numero_de_agujas) :
+    for _ in range (numero_agujas) :
+
+        # numeros aleatorios entre -1 y 1
         x = random.random() * random.choice ([-1, 1])
         y = random.random() * random.choice ([-1, 1])
+        
+        # calcular distancia del centro del circulo hacia el punto generado
         distancia_desde_el_centro = math.sqrt (x**2 + y**2)
-
         if (distancia_desde_el_centro <= 1) :
             dentro_del_circulo = dentro_del_circulo + 1
 
-    return (4 * dentro_del_circulo) /  numero_de_agujas
+    return (4 * dentro_del_circulo) /  numero_agujas
 
 
 
 ###################################################################
-def estimacion (numero_de_agujas, numero_de_intentos) :
+def estimacion (numero_agujas, numero_de_intentos) :
 ###################################################################
 
     estimados = []
     for _ in range (numero_de_intentos) :
-        porcentaje_agujas_dentro_del_circulo = aventar_agujas (numero_de_agujas)
+        porcentaje_agujas_dentro_del_circulo = aventar_agujas (numero_agujas)
         estimados.append (porcentaje_agujas_dentro_del_circulo)
         media_estimados = media (estimados)
         sigma = desviacion_estandar (estimados)
-        print ('Media_estimada: ', round (media_estimados, 5), ' Sigma: ', round (sigma, 5), ' Agujas: ', numero_de_agujas)
+        print ('Media_estimada: ', round (media_estimados, 5), ' Sigma: ', round (sigma, 5), ' Agujas: ', numero_agujas)
 
     return (media_estimados, sigma)
 
@@ -43,12 +47,12 @@ def estimacion (numero_de_agujas, numero_de_intentos) :
 def estimar_pi (precision, numero_de_intentos) :
 ###################################################################
 
-    numero_de_agujas = 1000
+    numero_agujas = 1000
     sigma = precision
 
     while sigma >= precision / 1.96 :    # para el 99.5% de confiabilidad en una distribucion normal
-        media, sigma = estimacion (numero_de_agujas, numero_de_intentos)
-        numero_de_agujas = numero_de_agujas * 2
+        media, sigma = estimacion (numero_agujas, numero_de_intentos)
+        numero_agujas = numero_agujas * 2
 
     return (media)
 
