b0d44b3b39a886f8a14e9be7703cfaee51bc19cc
Edgar Gonzalez Ambriz <egonzalezambriz@gmail.com>
2024-01-10 12:39:48 -0600

    eliminacion de numero_agujas como variable local en funcion estimar_pi dejanlola como argumento

--- calculo_pi.py
+++ calculo_pi.py
@@ -44,10 +44,9 @@ def estimacion (numero_agujas, numero_de_intentos) :
 
 
 ###################################################################
-def estimar_pi (precision, numero_de_intentos) :
+def estimar_pi (precision, numero_de_intentos, numero_agujas) :
 ###################################################################
 
-    numero_agujas = 1000
     sigma = precision
 
     while sigma >= precision / 1.96 :    # para el 99.5% de confiabilidad en una distribucion normal
@@ -59,5 +58,5 @@ def estimar_pi (precision, numero_de_intentos) :
 
 
 if __name__ == '__main__' :
-    estimar_pi (0.002, 300)     # Al revisar los resultados de las iteraciones es importante notar que sigma va disminuyendo
+    estimar_pi (0.002, 300, 1000)     # Al revisar los resultados de las iteraciones es importante notar que sigma va disminuyendo
 
